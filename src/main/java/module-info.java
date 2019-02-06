0889dea872d0af456f66a5f02528261da808c422
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-02-06 07:05:56 +0100

    [FEATURE] Externalize logback and splunk.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,10 +5,7 @@ module be.yildizgames.common.logging {
 
     requires be.yildizgames.common.exception;
     requires be.yildizgames.common.util;
-    requires splunk.library.javalogging;
-    requires logback.classic;
     requires slf4j.api;
-    requires logback.core;
 
     exports be.yildizgames.common.logging;
 }
