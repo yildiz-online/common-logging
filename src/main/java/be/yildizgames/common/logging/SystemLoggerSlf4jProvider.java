49cc8414e46086b692b41515f63a02fb0af19a81
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-08-17 14:23:43 +0300

    [WİP] toString

--- src/main/java/be/yildizgames/common/logging/SystemLoggerSlf4jProvider.java
+++ src/main/java/be/yildizgames/common/logging/SystemLoggerSlf4jProvider.java
@@ -41,5 +41,10 @@ public class SystemLoggerSlf4jProvider extends System.LoggerFinder {
     public final System.Logger getLogger(final String name, final Module module) {
         return new SystemLoggerSlf4j(LoggerFactory.getLogger(name));
     }
+    
+    @Override
+    public final String toString() {
+        return "System Logger SLF4J finder";
+    }
 
 }
