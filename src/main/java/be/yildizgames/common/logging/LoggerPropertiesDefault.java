593451fa65bfb6df47695fbf10f1586d2ab0e817
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-08-23 17:06:29 +0300

    [FIX] Logger name

--- src/main/java/be/yildizgames/common/logging/LoggerPropertiesDefault.java
+++ src/main/java/be/yildizgames/common/logging/LoggerPropertiesDefault.java
@@ -10,7 +10,7 @@ public class LoggerPropertiesDefault extends Properties {
     public LoggerPropertiesDefault() {
         this.setProperty(LoggerPropertiesConfiguration.LOGGER_LEVEL_KEY, "INFO");
         this.setProperty(LoggerPropertiesConfiguration.LOGGER_OUTPUT_KEY, "CONSOLE");
-        this.setProperty(LoggerPropertiesConfiguration.LOGGER_PATTERN_KEY, "%d{yyyy/MM/dd HH:mm:ss} | %level | %class | %msg%n");
+        this.setProperty(LoggerPropertiesConfiguration.LOGGER_PATTERN_KEY, "%d{yyyy/MM/dd HH:mm:ss} | %level | %logger | %msg%n");
         this.setProperty(LoggerPropertiesConfiguration.LOGGER_TCP_HOST_KEY, "localhost");
         this.setProperty(LoggerPropertiesConfiguration.LOGGER_TCP_PORT_KEY, "60000");
         this.setProperty(LoggerPropertiesConfiguration.LOGGER_FILE_OUTPUT_KEY, "log.log");
