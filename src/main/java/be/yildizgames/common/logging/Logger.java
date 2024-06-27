e594ebc05cd29ba7935581e1359188157e2261e6
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2024-06-27 18:47:14 +0200

    [WIP] Fix name.

--- src/main/java/be/yildizgames/common/logging/Logger.java
+++ src/main/java/be/yildizgames/common/logging/Logger.java
@@ -16,58 +16,121 @@
 package be.yildizgames.common.logging;
 
 /**
+ * This class provide an easy to use facade abode the System.Logger class.
+ *
  * @author Grégory Van den Borre
  */
 public class Logger {
 
-    private final System.Logger logger;
+    private final System.Logger log;
 
     private Logger(final String name) {
         super();
-        this.logger = System.getLogger(name);
+        this.log = System.getLogger(name);
     }
 
-    public static Logger getLogger(Class clazz) {
+    /**
+     * Create a new logger instance for the given class.
+     *
+     * @param clazz Class to get the logger for.
+     * @return The logger.
+     */
+    public static Logger getLogger(Class<?> clazz) {
         return new Logger(clazz.getName());
     }
 
+    /**
+     * Create a new logger instance for the given object.
+     *
+     * @param o Object to get the logger for.
+     * @return The logger.
+     */
     public static Logger getLogger(Object o) {
         return getLogger(o.getClass());
     }
 
+    /**
+     * Log an info message.
+     *
+     * @param message Message to log.
+     */
     public final void info(String message) {
-        this.logger.log(System.Logger.Level.INFO, message);
+        this.log.log(System.Logger.Level.INFO, message);
     }
 
+    /**
+     * Log an info message.
+     *
+     * @param format Message to log, it is expected to follow the java.text.MessageFormat format.
+     * @param params Parameters for the message format.
+     */
     public final void info(String format, Object... params) {
-        this.logger.log(System.Logger.Level.INFO, format, params);
+        this.log.log(System.Logger.Level.INFO, format, params);
     }
 
+    /**
+     * Log a warning message.
+     *
+     * @param message Message to log.
+     */
     public final void warning(String message) {
-        this.logger.log(System.Logger.Level.WARNING, message);
+        this.log.log(System.Logger.Level.WARNING, message);
     }
 
+    /**
+     * Log a warning message.
+     *
+     * @param format Message to log, it is expected to follow the java.text.MessageFormat format.
+     * @param params Parameters for the message format.
+     */
     public final void warning(String format, Object... params) {
-        this.logger.log(System.Logger.Level.WARNING, format, params);
+        this.log.log(System.Logger.Level.WARNING, format, params);
     }
 
+    /**
+     * Log an error message.
+     *
+     * @param message Message to log.
+     */
     public final void error(String message) {
-        this.logger.log(System.Logger.Level.ERROR, message);
+        this.log.log(System.Logger.Level.ERROR, message);
     }
 
+    /**
+     * Log an error message.
+     *
+     * @param format Message to log, it is expected to follow the java.text.MessageFormat format.
+     * @param params Parameters for the message format.
+     */
     public final void error(String format, Object... params) {
-        this.logger.log(System.Logger.Level.ERROR, format, params);
+        this.log.log(System.Logger.Level.ERROR, format, params);
     }
 
+    /**
+     * Log an error message.
+     *
+     * @param throwable Exception to log.
+     */
     public final void error(Throwable throwable) {
-        this.logger.log(System.Logger.Level.ERROR, "", throwable);
+        this.log.log(System.Logger.Level.ERROR, "", throwable);
     }
 
+    /**
+     * Log a debug message.
+     *
+     * @param message Message to log.
+     */
     public final void debug(String message) {
-        this.logger.log(System.Logger.Level.DEBUG, message);
+        this.log.log(System.Logger.Level.DEBUG, message);
     }
 
+    /**
+     * Log a debug message.
+     *
+     * @param format Message to log, it is expected to follow the java.text.MessageFormat format.
+     * @param params Parameters for the message format.
+     */
     public final void debug(String format, Object... params) {
-        this.logger.log(System.Logger.Level.DEBUG, format, params);
+        this.log.log(System.Logger.Level.DEBUG, format, params);
     }
 }
